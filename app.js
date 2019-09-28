aae8750366f3f1cd9d3a1d130290cf5802e89f42
Student <RVYakovenko@stud.kai.ru>
2019-09-28 10:57:17 +0300

    change name

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("print");
 
-const author = {
+const billieeilish = {
     id: 'billieeilish',
     avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/796b266b1e2b60e58a0c47039511d425/5E240D81/t51.2885-19/s320x320/67321025_476869713128764_5465198918969065472_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com',
     virified: true,
@@ -8,7 +8,7 @@ const author = {
 
 const photo = {
     id: 'B26_H4UF-pX',
-    author: author,
+    author: billieeilish,
     location: 'Manhattan, New York',
     imageUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/2943154402cd15d9d65551adf2eced9a/5E1CF288/t51.2885-15/sh0.08/e35/s640x640/69335204_147830689795181_4253221264260163107_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=1 640w,https://scontent-arn2-1.cdninstagram.com/vp/5dfe83596880f365398201bf64d16250/5E227588/t51.2885-15/sh0.08/e35/s750x750/69335204_147830689795181_4253221264260163107_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=1 750w,https://scontent-arn2-1.cdninstagram.com/vp/375298176e41e3fe81bf2326b0b52af3/5E1B3D3F/t51.2885-15/e35/s1080x1080/69335204_147830689795181_4253221264260163107_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=1 1080w',
     videoUrl: null,
@@ -17,7 +17,7 @@ const photo = {
 
 const video = {
     id: 'B1_6jPVFYPX',
-    author: author,
+    author: billieeilish,
     location: 'New York',
     likes: 1596366,
     imageUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/2943154402cd15d9d65551adf2eced9a/5E1CF288/t51.2885-15/sh0.08/e35/s640x640/69335204_147830689795181_4253221264260163107_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=1 640w,https://scontent-arn2-1.cdninstagram.com/vp/5dfe83596880f365398201bf64d16250/5E227588/t51.2885-15/sh0.08/e35/s750x750/69335204_147830689795181_4253221264260163107_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=1 750w,https://scontent-arn2-1.cdninstagram.com/vp/375298176e41e3fe81bf2326b0b52af3/5E1B3D3F/t51.2885-15/e35/s1080x1080/69335204_147830689795181_4253221264260163107_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com&_nc_cat=1 1080w',
